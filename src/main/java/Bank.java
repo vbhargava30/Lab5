760798cac09e36082cf7a72a17c8d56673bcd6e4
Varun Bhargava <varunb2@illinois.edu>
2018-02-21 02:55:16 -0600

    Initial class framework built with 90%+ of checkstyle issues removed.

--- src/main/java/Bank.java
+++ src/main/java/Bank.java
@@ -9,7 +9,15 @@
  */
 public class Bank {
 
-    public String bankName;
+    /**
+     * Name of the bank.
+     */
+    private String bankName;
+
+
+    /**
+     * Create a new bank.
+     */
 
     public Bank() {
         bankName = "Illini Bank";
@@ -26,9 +34,10 @@ public class Bank {
      * @return boolean
      */
     public boolean withdrawMoney(final BankAccount bankAccount, final double amount) {
-        /*
-         * Implement this function
-         */
+
+        
+
+
     }
 
     /**
@@ -74,21 +83,21 @@ public class Bank {
      */
 
     public void changeOwnerName(final BankAccount bankAccount, final String name) {
-        /*
-         * Implement this function
-         */
-    }
 
-    public static int totalAccounts = 0;
+    }
     /**
      * Uses static variable to get number of bank accounts opened.
      *
      * @return the total number of accounts
      */
+    public static int totalAccounts = 0;
+
+
+    /*
+     * Getter for the account number.
+     */
     public static int getNumberOfAccount() {
-        /*
-         * Implement this function
-         */
+
     }
 
     /**
--- src/main/java/BankAccount.java
+++ src/main/java/BankAccount.java
@@ -9,30 +9,166 @@ import java.util.Random;
  */
 public class BankAccount {
 
+    //Class and instance variables.
+
     /*
      * You may want to use this to distinguish between different kinds of accounts.
      */
     public enum BankAccountType {
+        /**
+         * A checking account.
+         */
         CHECKINGS,
+        /**
+         * A savings account.
+         */
         SAVINGS,
+        /**
+         * A student account.
+         */
         STUDENT,
+        /**
+         * A workplace account.
+         */
         WORKPLACE
     }
 
+    /**
+     * Account number.
+     */
     private int accountNumber;
-    public BankAccountType accountType;
+    /**
+     * Bank account type.
+     */
+    private BankAccountType accountType;
+    /**
+     * Bank account balance.
+     */
     private double accountBalance;
+    /**
+     * Bank account owner name.
+     */
     private String ownerName;
-    public double interestRate;
+    /**
+     * Bank account interest rate.
+     */
+    private double interestRate;
+
+    /**
+     * Bank account interest earned.
+     */
     private double interestEarned;
 
+    //Constructors.
+
+    /**
+     * Constructor to create a new bank account.
+     * @param name the name of the bank account holder.
+     * @param accountCategory the category of the bank account.
+     */
     public BankAccount(final String name, final BankAccountType accountCategory) {
         /*
          * Implement this function
          */
     }
 
-    /*
-     * Implement getters and setters as appropriate for private variables.
+    //Getters.
+
+    /**
+     * get the account number.
+     * @return account number.
      */
+    public int getAccountNumber() {
+        return accountNumber;
+    }
+
+    /**
+     * get the account type.
+     * @return account type.
+     */
+    public BankAccountType getAccountType() {
+        return accountType;
+    }
+
+    /**
+     * get the account balance.
+     * @return account balance.
+     */
+    public double getAccountBalance() {
+        return accountBalance;
+    }
+
+    /**
+     * get the owner name.
+     * @return the owner name.
+     */
+    public String getOwnerName() {
+        return ownerName;
+    }
+
+    /**
+     * get the interest rate.
+     * @return interest rate.
+     */
+    public double getInterestRate() {
+        return interestRate;
+    }
+
+    /**
+     * get the interest earned.
+     * @return the interest earned.
+     */
+    public double getInterestEarned() {
+        return interestEarned;
+    }
+
+    //Setters.
+
+    /**
+     * Set the account number.
+     * @param accountNumber the account number to set.
+     */
+    public void setAccountNumber(int accountNumber) {
+        this.accountNumber = accountNumber;
+    }
+
+    /**
+     * Set the account type.
+     * @param accountType the account type to set.
+     */
+    public void setAccountType(BankAccountType accountType) {
+        this.accountType = accountType;
+    }
+
+    /**
+     * Set the account balance.
+     * @param accountBalance the account balance to set.
+     */
+    public void setAccountBalance(double accountBalance) {
+        this.accountBalance = accountBalance;
+    }
+
+    /**
+     * Set the owner name.
+     * @param ownerName The owner name to set.
+     */
+    public void setOwnerName(String ownerName) {
+        this.ownerName = ownerName;
+    }
+
+    /**
+     * Set the interest rate.
+     * @param interestRate the interest rate to set.
+     */
+    public void setInterestRate(double interestRate) {
+        this.interestRate = interestRate;
+    }
+
+    /**
+     * Set the interest earned.
+     * @param interestEarned the interest earned to set.
+     */
+    public void setInterestEarned(double interestEarned) {
+        this.interestEarned = interestEarned;
+    }
 }
